ca452b2fe6ac0c64b89abe1b9c04f42736a3033b
linre-90 <juho.lindemark@gmail.com>
2023-03-11 09:10:12 +0200

    Saved selected theme to local storage

--- src/main/resources/static/navbar.js
+++ src/main/resources/static/navbar.js
@@ -21,15 +21,29 @@ function displayMobileNav(){
 
 /** Toggles theme */
 function setTheme(mode){
-
     if(mode === "light"){
         if(!document.body.classList.contains("light")){
             document.body.classList.add("light");
+            localStorage.setItem("theme", "light");
         }
     }
     if(mode === "dark"){
         if(document.body.classList.contains("light")){
             document.body.classList.remove("light");
+            localStorage.setItem("theme", "dark")
         }
     }
 }
+
+/** Try to load selected theme from local storage */
+function getTheme(){
+    const theme = localStorage.getItem("theme");
+    if(theme === null){
+        setTheme("dark");
+        return;
+    }
+
+    setTheme(theme);
+}
+
+getTheme();
